a83af8d446771111b3b9cab0ffd8b1c45b150f0b
annammattis <54290779+annammattis@users.noreply.github.com>
2020-01-08 12:54:13 -0500

    added variables

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.SPOTIFY_ID,'329fdbcd44fa4abf804382f64b09f4b2'
+  secret: process.env.SPOTIFY_SECRET '96dd46084be14fe0a12c6112ec51cedb'
 };
--- liri.js
+++ liri.js
@@ -3,11 +3,53 @@ require("dotenv").config();
 // add the code required to import the keys.js file and store it in a variable
 var keys = require("./keys.js");
 // should then be able to access your keys information like so..
-var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+
+
 
 
 // Make it so liri.js can take in one of the following commands:
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
+var concertThis = function(artist) {
+    var region = "";
+    var queryUrl = "";
+}
+
+// ------------------------------------------------------------------------
+var spotifyThisSong = function(song) {
+    if (!song) {
+        //default is I Want it That Way by Backstreet Boys
+        song = "I Want it That Way"
+    }
+}
+
+var spotify = new Spotify(keys.spotify);
+spotify.search({ type: 'track', query: song, limit: 1}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
+
+//   ------------------------------------------------------------------------
+
+var movieThis = function(movie) {
+
+}
+
+var queryUrl = "";
+
+
+
+var doWhatItSays = function() {
+    fs.readfile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err)
+        }
+
+        var dataArr = data.split(",")
+        //call appropriate arguement and pass function
+        runAction(dataArr[0], dataArr[1])
+    });
+}
+
