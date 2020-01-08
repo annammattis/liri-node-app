7e4cd6a796658a6d66974989312e81d03ad9fb63
annammattis <54290779+annammattis@users.noreply.github.com>
2020-01-08 12:22:12 -0500

    update 2

---
+++ liri.js
@@ -0,0 +1,12 @@
+// add code to read and set any environment variables with the dotenv package:
+require("dotenv").config();
+// add the code required to import the keys.js file and store it in a variable
+var keys = require("./keys.js");
+// should then be able to access your keys information like so..
+var spotify = new Spotify(keys.spotify);
+
+// Make it so liri.js can take in one of the following commands:
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
