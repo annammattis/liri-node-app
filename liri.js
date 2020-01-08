b569dea23bc4b00f1dabaecb2a18eb7fbe3eac3c
annammattis <54290779+annammattis@users.noreply.github.com>
2020-01-08 18:08:04 -0500

    lirijs update

--- liri.js
+++ liri.js
@@ -3,15 +3,16 @@ require("dotenv").config();
 // add the code required to import the keys.js file and store it in a variable
 var keys = require("./keys.js");
 // should then be able to access your keys information like so..
-var Spotify = require('node-spotify-api');
-var axios = require('axios');
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var fs = require("fs");
 
 
 
 // Make it so liri.js can take in one of the following commands:
 var concertThis = function(artist) {
     var region = "";
-    var queryUrl = "";
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 }
 
 // ------------------------------------------------------------------------
@@ -34,10 +35,15 @@ spotify.search({ type: 'track', query: song, limit: 1}, function(err, data) {
 //   ------------------------------------------------------------------------
 
 var movieThis = function(movie) {
-
+    //default will be Mr. Nobody
+    if (!movie) {
+        movie = "Mr. Nobody"
+    }
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 }
 
-var queryUrl = "";
+
 
 
 
