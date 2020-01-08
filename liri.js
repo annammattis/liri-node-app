3252993403bfe4d9cbac51f2b5b356bf6d3ea2e5
annammattis <54290779+annammattis@users.noreply.github.com>
2020-01-08 12:26:30 -0500

    npm spotify

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ var keys = require("./keys.js");
 // should then be able to access your keys information like so..
 var spotify = new Spotify(keys.spotify);
 
+
 // Make it so liri.js can take in one of the following commands:
 // concert-this
 // spotify-this-song
